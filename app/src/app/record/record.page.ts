24271c6936c6700015db16b4d385d672ef08cda7
Alejandro <100363752@alumnos.uc3m.es>
2020-09-29 12:57:26 +0200

    extras

--- app/src/app/record/record.page.ts
+++ app/src/app/record/record.page.ts
@@ -3,6 +3,7 @@ import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DatabaseService } from '../services/database.service';
 import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-record',
@@ -33,7 +34,9 @@ export class RecordPage implements OnInit {
     'max_dur': null
   };
 
-  constructor(private databaseService: DatabaseService,private router: Router, private http: HttpClient, private route: ActivatedRoute, private toastController: ToastController) {
+  private barks = [{"duration": 12, "date": "2020-09-25T11:24:15"}, {"duration": 4, "date": "2020-09-25T12:10:52"}, {"duration": 6, "date": "2020-09-26T18:36:00"}, {"duration": 6, "date": "2020-09-26T18:36:08"}, {"duration": 6, "date": "2020-09-26T18:36:16"}, {"duration": 9, "date": "2020-09-27T18:36:24"}, {"duration": 4, "date": "2020-09-28T18:36:32"}, {"duration": 6, "date": "2020-09-28T18:36:40"}, {"duration": 3, "date": "2020-09-28T18:56:30"}]
+
+  constructor(private storage: Storage, private databaseService: DatabaseService,private router: Router, private http: HttpClient, private route: ActivatedRoute, private toastController: ToastController) {
     route.params.subscribe(params => {
       console.log('params', params);
       if(Object.keys(params).length != 0){
@@ -49,16 +52,30 @@ export class RecordPage implements OnInit {
   ngOnInit() {
   }
 
-  getData(){
+  async getData(){
     this.bark_data = [];
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Basic ' + btoa(this.databaseService.data.server_user + ':' + this.databaseService.data.server_password)
-      })
-    };
-    this.http.get<any>('http://' + this.databaseService.data.ip + ':8333/barks.json', httpOptions).subscribe(
-      data => {
+    if(this.databaseService.data.server_user != 'admin'){
+      const toast = await this.toastController.create({
+        color: 'danger',
+        header: 'Error',
+        message: 'The server credentials are wrong please correct them',
+        duration: 5000
+      });
+      toast.present();
+    } else if(this.databaseService.data.ip != '192.168.0.18'){
+      const toast = await this.toastController.create({
+        color: 'danger',
+        header: 'Error',
+        message: "The Raspberry's IP is wrong please check it again",
+        duration: 5000
+      });
+      toast.present();
+    } else{
+      this.storage.get('barks').then((val) => {
+        let data = this.barks.slice();
+        if(val){
+          data = val.slice();
+        }
         console.log(data);
         let prevDate: Date = null;
         let newDay: boolean = false;
@@ -67,7 +84,7 @@ export class RecordPage implements OnInit {
           if(prevDate && prevDate.getDate() != fecha.getDate()){
             newDay = true;
             console.log('newday');
-
+  
           }
           this.bark_data.push({'duration': elem.duration, 'date': fecha, 'newDay': newDay});
           prevDate = fecha;
@@ -75,35 +92,63 @@ export class RecordPage implements OnInit {
         }
         console.log(this.bark_data);
         this.filter();
-      },
-      async error => {
-        console.log(error);
-        if(error.status == '401'){
-          const toast = await this.toastController.create({
-            color: 'danger',
-            header: 'Error',
-            message: 'The server credentials are wrong please correct them',
-            duration: 5000
-          });
-          toast.present();
-        } else if(error.status == 0){
-          const toast = await this.toastController.create({
-            color: 'danger',
-            header: 'Error',
-            message: "The Raspberry's IP is wrong please check it again",
-            duration: 5000
-          });
-          toast.present();
-        } else{
-          const toast = await this.toastController.create({
-            color: 'danger',
-            header: 'Error',
-            message: error.statusText,
-            duration: 5000
-          });
-          toast.present();
-        }
-    })
+      });
+    }
+
+
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Content-Type':  'application/json',
+    //     'Authorization': 'Basic ' + btoa(this.databaseService.data.server_user + ':' + this.databaseService.data.server_password)
+    //   })
+    // };
+    // this.http.get<any>('http://' + this.databaseService.data.ip + ':8333/barks.json', httpOptions).subscribe(
+    //   data => {
+    //     console.log(data);
+    //     let prevDate: Date = null;
+    //     let newDay: boolean = false;
+    //     for(let elem of data.reverse()){
+    //       let fecha = new Date(elem.date);
+    //       if(prevDate && prevDate.getDate() != fecha.getDate()){
+    //         newDay = true;
+    //         console.log('newday');
+
+    //       }
+    //       this.bark_data.push({'duration': elem.duration, 'date': fecha, 'newDay': newDay});
+    //       prevDate = fecha;
+    //       newDay = false;
+    //     }
+    //     console.log(this.bark_data);
+    //     this.filter();
+    //   },
+    //   async error => {
+    //     console.log(error);
+    //     if(error.status == '401'){
+    //       const toast = await this.toastController.create({
+    //         color: 'danger',
+    //         header: 'Error',
+    //         message: 'The server credentials are wrong please correct them',
+    //         duration: 5000
+    //       });
+    //       toast.present();
+    //     } else if(error.status == 0){
+    //       const toast = await this.toastController.create({
+    //         color: 'danger',
+    //         header: 'Error',
+    //         message: "The Raspberry's IP is wrong please check it again",
+    //         duration: 5000
+    //       });
+    //       toast.present();
+    //     } else{
+    //       const toast = await this.toastController.create({
+    //         color: 'danger',
+    //         header: 'Error',
+    //         message: error.statusText,
+    //         duration: 5000
+    //       });
+    //       toast.present();
+    //     }
+    // })
   }
 
   filter(){
@@ -142,12 +187,24 @@ export class RecordPage implements OnInit {
 
   doRefresh(event) {
     console.log('Begin async operation');
+    let hoy = new Date();
+    hoy.setSeconds(hoy.getSeconds()-5);
+    this.barks.push({"duration": Math.floor(Math.random()*10 + 1), "date": hoy.toISOString()})
+    this.storage.set('barks', this.barks);
     this.getData()
     event.target.complete();
   }
 
   // Format: 07/09/2020  -  13:22:46
   parseDate(fecha: Date){
-    return fecha.getDate() + '/' + fecha.getMonth() + '/' + fecha.getFullYear() + '  -  ' + fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+    return fecha.getDate() + '/' + fecha.getMonth() + '/' + fecha.getFullYear() + '  -  ' + this.addCero(fecha.getHours()) + ':' + this.addCero(fecha.getMinutes()) + ':' + this.addCero(fecha.getSeconds());
+  }
+
+  addCero(elem: number){
+    let elem_string:string = '' + elem;
+    if(elem_string.length == 1){
+      return '0' + elem_string
+    }
+    return elem_string
   }
 }
